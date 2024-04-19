e3e89045e10e1c85770690894750f2e8e59cb5a0
novil Johnson <noviljohnson9@gmail.com>
2024-04-19 17:39:26 +0530

    added qa chain

--- summarizer.py
+++ summarizer.py
@@ -16,6 +16,7 @@ from langchain_community.embeddings.sentence_transformer import (
     SentenceTransformerEmbeddings,
 )
 from langchain_core.documents import Document
+from langchain.chains.question_answering import load_qa_chain
 
 import sqlite3
 from flask import Flask
@@ -39,14 +40,14 @@ embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2"
 
 
 checkpoint = "./LaMini-Flan-T5-248M"
-tokenizer = T5Tokenizer.from_pretrained(checkpoint )
+tokenizer = T5Tokenizer.from_pretrained(checkpoint, max_length=1024, truncation=True )
 base_model = T5ForConditionalGeneration.from_pretrained(checkpoint , device_map='auto', offload_folder="offload",torch_dtype = torch.float32)
-
+print(f"\n ==== {tokenizer.model_max_length} ==== \n")
 pipe = pipeline(
     "summarization",
     model = base_model,
     tokenizer=tokenizer,
-    max_length = 500,
+    max_length = 1000,
     min_length = 50
 )
 
@@ -67,6 +68,48 @@ title_prompt = PromptTemplate.from_template(title_template)
 
 chain = prompt | hf
 title_chain = title_prompt | hf
+
+
+#### QA Pipeline
+
+model_name = "Intel/dynamic_tinybert"
+
+# Load the tokenizer associated with the specified model
+tokenizer = AutoTokenizer.from_pretrained(model_name, padding=True, truncation=True, max_length=1024)
+print(f"\n 2222==== {tokenizer.model_max_length} ==== \n")
+# Define a question-answering pipeline using the model and tokenizer
+question_answerer = pipeline(
+    "question-answering", 
+    model=model_name, 
+    tokenizer=tokenizer,
+    return_tensors='pt'
+)
+
+# Create an instance of the HuggingFacePipeline, which wraps the question-answering pipeline
+# with additional model-specific arguments (temperature and max_length)
+llm = HuggingFacePipeline(
+    pipeline=question_answerer,
+    model_kwargs={"temperature": 0.2, "max_length": 1024},
+)
+
+prompt_template = """Use the following pieces of context to answer the question at the end. 
+If the answer can be inferred from the context, provide it with proper punctuation and capitalization. 
+If the answer is not present in the context but can be addressed with your model knowledge up to your last update, use that information to answer. 
+If you still don't know the answer, state that you don't know. Do not fabricate an answer.
+
+{context}
+
+Question: {question}
+Generate point-wise answers for the question , and format them clearly with bullet points or numbers.
+Conclude or explain the context based on the question's intent when possible.
+If the answer is not present in the context, attempt to provide a close enough answer using the model's knowledge without making assumptions beyond that knowledge.
+Answer:"""
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]#, "sources"]
+)
+
+qa_chain=load_qa_chain(llm,  chain_type="stuff", prompt=PROMPT)
+
 #######################################################################
 
 
@@ -142,9 +185,11 @@ class Summer:
             self.vectorDB = Chroma(persist_directory="./chroma_db", embedding_function=embedding_function, collection_name="collection_"+str(id))
             self.vectorDB.add_documents(docs)
 
-    def query_VectorDb(self, query):
+    def query_VectorDb(self, query, id):
+        print("\nid===",str(id), "\n")
         self.vectorDB = Chroma(persist_directory="./chroma_db", embedding_function=embedding_function, collection_name="collection_"+str(id))
-        docs = self.vectorDB.as_retriever().get_relevant_documents(query)
+        retriever = self.vectorDB.as_retriever(search_type="mmr",search_kwargs={"k": 4})
+        docs = retriever.get_relevant_documents(query)
         return docs
 
 
@@ -188,8 +233,18 @@ def get_summary():
     docs = obj.create_docs(user_input['text'], title)
     obj.addDocs_2_vetordb(docs, user_details[0][0])
 
-    summary = chain.invoke({"input_text": user_input['text']})
-    
+    try:
+        summary = chain.invoke({"input_text": user_input['text']})
+    except Exception as e:
+        summary = ''
+        print(e)
+        
+    if len(user_input['text']) < 500:
+        docs = obj.query_VectorDb(user_input['text'], user_details[0][0])
+        qa_chain_res = qa_chain({"input_documents": docs, 
+                        "question":[ user_input['text']]})
+        
+        print(qa_chain_res['output_text'])
     return jsonify({'summary':summary}), 200
 
 
